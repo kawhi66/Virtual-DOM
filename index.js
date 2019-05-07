3b1c1e26087789424700c1961168b4f665f702bd
kawhi <kawhi_site@163.com>
2019-05-07 10:24:01 +0800

    stuck in Model.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('hello Virtual DOM');
+const h = require('./lib/create')
+const div = h('div', {}, 'hello')
+const ul = h('ul', {}, [h('li', {}, 'item - 1'), h('li', {}, 'item - 2')])
+
+document.body.insertBefore(div, document.body.lastElementChild)
+document.body.insertBefore(ul, document.body.lastElementChild)
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    context: path.resolve(__dirname),
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        hot: true,
+        open: true,
+        port: 3001
+    },
+    entry: {
+        main: './index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: []
+    },
+    resolve: {},
+    plugins: [
+        new HtmlWebpackPlugin({
+            favicon: './public/favicon.ico',
+            template: './public/index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
