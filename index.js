38f1bf0e92bcec92ac831e573c838d61732d8268
kawhi <kawhi_site@163.com>
2019-05-09 14:20:14 +0800

    update models

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const createElement = require('./lib/create')
 const diff = require('./lib/diff')
 const diffProps = require('./lib/diff').diffProps
 const h = require('./lib/hyperscript')
-const VNode = require('./lib/vnode')
-const VText = require('./lib/vtext')
+const VNode = require('./lib/model/vnode')
+const VText = require('./lib/model/vtext')
 
 module.exports = {
     createElement,
