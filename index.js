497636e1762390a068fa82c6575efc9ad4efc43e
kawhi <kawhi_site@163.com>
2019-05-09 12:04:15 +0800

    diff

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const createElement = require('./lib/create')
+const diff = require('./lib/diff')
+const diffProps = require('./lib/diff').diffProps
 const h = require('./lib/hyperscript')
 const VNode = require('./lib/vnode')
 const VText = require('./lib/vtext')
 
 module.exports = {
     createElement,
+    diff,
+    diffProps,
     h,
     VNode,
     VText
