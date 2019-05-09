0831d9774b58b5609c9b6d1396fc24f22356ebe9
kawhi <kawhi_site@163.com>
2019-05-09 18:10:23 +0800

    update example

--- example/src/index.js
+++ example/src/index.js
@@ -1,6 +1,8 @@
 import {
     createElement,
-    h
+    diff,
+    h,
+    patch
 } from 'VirtualDOM'
 
 const scriptNode = document.getElementsByTagName('script')[0];
@@ -21,3 +23,72 @@ document.body.insertBefore(createElement(h('div', h('a', {
 document.body.insertBefore(createElement(h('div', h('a', {
     href: '/case4.html'
 }, '4、子元素未知宽高(或已知宽高)，父子元素基于 table-cell 和 inline-block 实现'))), scriptNode)
+
+
+document.body.insertBefore(createElement(h('h1', {
+    style: {
+        'margin-top': '50px'
+    }
+}, '一个有趣的例子，猜到 100 的时候会发生什么')), scriptNode)
+
+// 1: Create a function that declares what the DOM should look like
+function render(count) {
+    return h('div', {
+        style: {
+            textAlign: 'center',
+            lineHeight: (25 + count) + 'px',
+            border: '1px solid red',
+            width: (25 + count) + 'px',
+            height: (25 + count) + 'px'
+        }
+    }, [String(count)]);
+}
+
+// 2: Initialise the document
+var count = 0; // We need some app data. Here we just store a count.
+
+var tree = render(count); // We need an initial tree
+var rootNode = createElement(tree); // Create an initial root DOM node ...
+document.body.insertBefore(rootNode, scriptNode); // ... and it should be in the document
+
+// 3: Wire up the update logic
+setInterval(function () {
+    count++;
+
+    var newTree = render(count);
+    var patches = diff(tree, newTree);
+    rootNode = patch(rootNode, patches);
+    tree = newTree;
+}, 1000);
+
+document.body.insertBefore(createElement(h('pre', [h('code', `
+// 1: Create a function that declares what the DOM should look like
+function render(count) {
+    return h('div', {
+        style: {
+            textAlign: 'center',
+            lineHeight: (25 + count) + 'px',
+            border: '1px solid red',
+            width: (25 + count) + 'px',
+            height: (25 + count) + 'px'
+        }
+    }, [String(count)]);
+}
+
+// 2: Initialise the document
+var count = 0; // We need some app data. Here we just store a count.
+
+var tree = render(count); // We need an initial tree
+var rootNode = createElement(tree); // Create an initial root DOM node ...
+document.body.insertBefore(rootNode, scriptNode); // ... and it should be in the document
+
+// 3: Wire up the update logic
+setInterval(function () {
+    count++;
+
+    var newTree = render(count);
+    var patches = diff(tree, newTree);
+    rootNode = patch(rootNode, patches);
+    tree = newTree;
+}, 1000);
+`)])), scriptNode)
