954250c4a7296406446b55581683f4133877d06c
kawhi <kawhi_site@163.com>
2019-05-09 15:10:44 +0800

    patch

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const createElement = require('./lib/create')
 const diff = require('./lib/diff')
 const diffProps = require('./lib/diff').diffProps
 const h = require('./lib/hyperscript')
-const VNode = require('./lib/model/vnode')
-const VText = require('./lib/model/vtext')
+const patch = require('./lib/patch')
+const VNode = require('./lib/models/vnode')
+const VText = require('./lib/models/vtext')
 
 module.exports = {
     createElement,
     diff,
     diffProps,
     h,
+    patch,
     VNode,
     VText
 }
